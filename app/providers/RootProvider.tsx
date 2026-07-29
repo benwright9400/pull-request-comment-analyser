7449a5c1b82ca02e03306b8018c0188043d25c52
Ben Wright <benwright9400@gmail.com>
2026-07-29 02:19:16 +0100

    cleanup: remove old code

--- app/providers/RootProvider.tsx
+++ app/providers/RootProvider.tsx
@@ -1,18 +1,15 @@
 "use client";
 
 import { ReactNode } from "react";
-import { DrawerProvider } from "./child-providers/DrawerProvider";
 import { NextAuthSessionProvider } from "./child-providers/SessionProvider";
 import { ConfirmationDialogueProvider } from "./child-providers/ConfirmationDialogProvider";
 
 export default function RootProvider({ children }: { children: ReactNode }) {
     return (
         <NextAuthSessionProvider>
-            <DrawerProvider>
-                <ConfirmationDialogueProvider>
-                    {children}
-                </ConfirmationDialogueProvider>
-            </DrawerProvider>
+            <ConfirmationDialogueProvider>
+                {children}
+            </ConfirmationDialogueProvider>
         </NextAuthSessionProvider>
     );
 }
